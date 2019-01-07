ed35a5db37e1bce46186ae482885f7b4ca097e0c
williamperezbeltran <williampbeltranprogramador@gmail.com>
2019-01-07 14:09:22 -0500

    fix error css loading

--- project1/settings.py
+++ project1/settings.py
@@ -132,3 +132,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+os.path.dirname(os.path.abspath(__file__))
+
